c7dfe73a71c3d8d312db17f0ec3855995ba610ad
Femi <oluwafemi.za@gmail.com>
2022-08-01 15:04:54 +0100

    Made progress smoother and added track colour

--- app/src/main/java/com/femi/a2fa/MainActivity.kt
+++ app/src/main/java/com/femi/a2fa/MainActivity.kt
@@ -8,23 +8,25 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
+import androidx.dynamicanimation.animation.SpringAnimation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.lifecycleScope
 import com.femi.a2fa.databinding.ActivityMainBinding
 import com.google.firebase.crashlytics.buildtools.reloc.org.apache.commons.codec.binary.Base32
 import com.google.firebase.crashlytics.buildtools.reloc.org.apache.commons.codec.binary.Hex
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import java.security.SecureRandom
+import kotlin.math.roundToInt
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val progress = MutableLiveData(-1)
+//    private lateinit var getProgressJob: Job
 
 //    https://medium.com/@ihorsokolyk/two-factor-authentication-with-java-and-google-authenticator-9d7ea15ffee6
 //    https://github.com/taimos/totp/blob/master/src/main/java/de/taimos/totp/TOTP.java
@@ -40,28 +42,30 @@ class MainActivity : AppCompatActivity() {
 
         newKey()
 
-        lifecycleScope.launch {
-            while (true) {
-                progress.value = (((30 - TOTP.getTimeRemaining()) * 3.33).toInt())
-                delay(100)
+        if (!getProgress().isActive)
+            getProgress()
+
+        progress.observe(this) {
+            ObjectAnimator.ofInt(binding.remainingTimeBar, "progress", it)
+                .setDuration(1000)
+                .start()
+            getTOTPCode()
+            when {
+                (it in 0..49) -> binding.remainingTimeBar.trackColor = getColor(R.color.green)
+                (it in 49..75) -> binding.remainingTimeBar.trackColor = getColor(R.color.yellow)
+                (it in 75..100) -> binding.remainingTimeBar.trackColor = getColor(R.color.red)
             }
         }
-
-        progress.observe(this){
-            binding.remainingTimeBar.progress = it
-        }
+        val cm: ClipboardManager =
+            getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
 
         binding.secretKey.setOnClickListener {
-            val cm: ClipboardManager =
-                getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
             val cData = ClipData.newPlainText("Secret Key", binding.secretKey.text)
             cm.setPrimaryClip(cData)
             Toast.makeText(this, "Copied key to clipboard", Toast.LENGTH_SHORT).show()
         }
 
         binding.secretOTP.setOnClickListener {
-            val cm: ClipboardManager =
-                getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
             val cData = ClipData.newPlainText("Secret OTP", binding.secretOTP.text)
             cm.setPrimaryClip(cData)
             Toast.makeText(this, "Copied OTP to clipboard", Toast.LENGTH_SHORT).show()
@@ -69,6 +73,13 @@ class MainActivity : AppCompatActivity() {
 
         binding.fab.setOnClickListener {
             newKey()
+        }
+    }
+
+    private fun getProgress() = lifecycleScope.launch {
+        while (isActive) {
+            progress.value = (((30 - TOTP.getTimeRemaining()) * 3.33).roundToInt())
+            delay(1000)
         }
     }
 
